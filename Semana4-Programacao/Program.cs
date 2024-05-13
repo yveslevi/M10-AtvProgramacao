fe5777cfddf33d5b7db3cdeb0c83bdeecb123e8c
Yves Levi <Yves.Lapa@sou.inteli.edu.br>
2024-05-12 22:10:11 -0300

    init project

---
+++ Semana4-Programacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+using System.Threading;
+
+class Program
+{
+    static Meter s_meter = new Meter("HatCo.Store");
+    static Counter<int> s_hatsSold = s_meter.CreateCounter<int>("hatco.store.hats_sold");
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Press any key to exit");
+        while (!Console.KeyAvailable)
+        {
+            // Pretend our store has a transaction each second that sells 4 hats
+            Thread.Sleep(1000);
+            s_hatsSold.Add(4);
+        }
+    }
+}
