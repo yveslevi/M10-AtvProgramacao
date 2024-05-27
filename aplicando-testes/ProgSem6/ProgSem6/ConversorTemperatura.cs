d69b816424eb1adbe96f99b1604620d2fa9bff2c
Yves Levi <Yves.Lapa@sou.inteli.edu.br>
2024-05-27 11:16:19 -0300

    feat: add xunit tests for temperature

---
+++ aplicando-testes/ProgSem6/ProgSem6/ConversorTemperatura.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualBasic;
+using System;
+
+namespace Temperatura
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Working!");
+        }
+    }
+
+    public static class ConversorTemperatura
+    {
+        public static double FahrenheitParaCelsius(double temperatura)
+            => Math.Round((temperatura - 32) / 1.8, 2);
+    }
+}
