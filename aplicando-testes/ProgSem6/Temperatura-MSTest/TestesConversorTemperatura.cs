bf5c177577631ada09c457c1d505c4a855a07910
Yves Levi <Yves.Lapa@sou.inteli.edu.br>
2024-05-27 11:26:38 -0300

    feat: add tests with mstest

---
+++ aplicando-testes/ProgSem6/Temperatura-MSTest/TestesConversorTemperatura.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Temperatura;
+
+namespace Temperatura_MSTest
+{
+    [TestClass]
+    public class TestesConversorTemperatura
+    {
+        [DataRow(32, 0)]
+        [DataRow(47, 8.33)]
+        [DataRow(86, 30)]
+        [DataRow(90.5, 32.5)]
+        [DataRow(120.18, 48.99)]
+        [DataRow(212, 100)]
+        [DataTestMethod]
+        public void TesteConversaoTemperatura(
+            double tempFahrenheit, double tempCelsius)
+        {
+            double valorCalculado =
+                ConversorTemperatura.FahrenheitParaCelsius(tempFahrenheit);
+            Assert.AreEqual(tempCelsius, valorCalculado);
+        }
+    }
+}
